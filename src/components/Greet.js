05fe0141b08c0584273422fcdb8cd3042bec8450
Piriyalini Niththiyanantham <priyalini@unicomtechnology.co.uk>
2023-03-01 09:33:45 +0530

    React Functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Priya</h1>
+// }
+
+// ES6 arrow function
+const Greet = () => <h1>Hello Priya!</h1>
+
+// Default export
+export default Greet;
