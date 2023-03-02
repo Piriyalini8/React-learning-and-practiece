bbb563c12489aa91955d3d66f266308f6863df31
Piriyalini Niththiyanantham <priyalini@unicomtechnology.co.uk>
2023-03-02 05:35:57 +0530

    React State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
@@ -12,18 +13,21 @@ function App() {
       {/* <Hello/> */}
 
       {/* props with functional components */}
-      <Greet name="Priya" heroName="Wonder Women">
+      {/* <Greet name="Priya" heroName="Wonder Women">
         <p>This is children props</p>
       </Greet>
       <Greet name="Kishan" heroName="Superman">
         <button>Action</button>
       </Greet>
-      <Greet name="Pavi" heroName="Batman"/>
+      <Greet name="Pavi" heroName="Batman"/> */}
 
       {/* props with class components */}
-      <Welcome name="Priya" heroName="Wonder Women"/>
+      {/* <Welcome name="Priya" heroName="Wonder Women"/>
       <Welcome name="Kishan" heroName="Superman"/>
-      <Welcome name="Pavi" heroName="Batman"/>
+      <Welcome name="Pavi" heroName="Batman"/> */}
+
+      {/* State in class example */}
+      <Message/>
 
     </div>
   );
