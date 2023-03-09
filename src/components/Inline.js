bb915ee95660e2b1e3e3b008e097f756da5622db
Piriyalini Niththiyanantham <priyalini@unicomtechnology.co.uk>
2023-03-09 14:59:58 +0530

    Styling and Css basics

---
+++ src/components/Inline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const heading= {
+    fontSize: '72px',
+    color: 'blue'
+}
+
+// {/* Use Inline styling */}
+function Inline() {
+  return (
+    <div style={heading}>Inline</div>
+  )
+}
+
+export default Inline
